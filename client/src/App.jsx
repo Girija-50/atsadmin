1f73c93daf87fe1b72ca5a1e2aba4d9a368c2462
Girija-50 <kaviya55984@gmail.com>
2026-05-23 20:47:14 +0530

    Fixed component imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,16 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/resumes" element={<YourResumes />} />
+
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+
+        <Route
+          path="/resumes"
+          element={<YourResumes />}
+        />
       </Routes>
     </BrowserRouter>
   );
